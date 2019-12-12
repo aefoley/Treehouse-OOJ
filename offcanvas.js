e9d7c0c8aa2edf1062b1875a76709220294eb813
Ann Foley <ann@prairieopolis.com>
2019-12-12 15:22:39 -0600

    the class is working

--- offcanvas.js
+++ offcanvas.js
@@ -5,3 +5,24 @@ $(function () {
     $('.offcanvas-collapse').toggleClass('open')
   })
 })
+
+class Pet {
+  constructor(animal, age, breed, sound) {
+    this.animal = animal;
+    this.age = age;
+    this.breed = breed;
+    this.sound = sound;
+  }
+  
+  speak(){
+   console.log(this.sound);
+   
+  }
+}
+
+
+const ernie = new Pet('dog', 1, 'pug', 'woof!');
+const vera = new Pet('cat', 5, 'tabby', 'meow');
+
+console.log(ernie);
+ernie.speak();
